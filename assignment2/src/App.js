aaf9804ca1a6f1363a342e313524df6d572dc280
Viral Shsatri <shastriv19@gmail.com>
2020-04-19 21:41:10 +0530

    ValidationComponent Done

--- assignment2/src/App.js
+++ assignment2/src/App.js
@@ -1,21 +1,37 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import ValidationComponent from "./ValidationComponent/ValidationComponent";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    state = {
+        string: "",
+    };
+
+    stringValidator = event => {
+        this.setState({
+            string: event.target.value,
+        });
+    };
+
+    characterCounter = () => {};
+    render() {
+        let validationComponent = null;
+        if (this.state.string.length < 5) {
+            validationComponent = (
+                <ValidationComponent string="Text too short" />
+            );
+        } else {
+            validationComponent = (
+                <ValidationComponent string="Text Long Enough" />
+            );
+        }
+        return (
+            <div className="App">
+                <input onChange={this.stringValidator} />
+                {validationComponent}
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ assignment2/src/ValidationComponent/ValidationComponent.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const validationComponent = props => {
+    return <p>{props.string}</p>;
+};
+
+export default validationComponent;
